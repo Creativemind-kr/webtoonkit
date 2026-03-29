84d5658339a4d12036cb0e9d5127a55314f681ed
notion-user <a01024125796@gmail.com>
2026-03-30 08:12:16 +0900

    fix: Firebase 빌드 오류 수정

--- src/app/partner/apply/page.tsx
+++ src/app/partner/apply/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState } from "react";
 import Link from "next/link";
 import { doc, updateDoc, collection, addDoc } from "firebase/firestore";
